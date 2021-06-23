968a95a31cdc57c6de3ed0edfe5e425037a05b4c
Jake <jhutchinson.programmer@gmail.com>
2021-06-23 13:15:44 -0400

    Finished Bot
    Bot is finished needs to have a few changes done to code to suit other servers. (Chat ids and file locations)

--- birthdaybot.py
+++ birthdaybot.py
@@ -1,42 +1,84 @@
 import discord
 from discord.ext import commands, tasks
+from discord.ext.commands import bot
 from datetime import datetime
 import os
-import re
 from dotenv import load_dotenv
 load_dotenv()
 
-def initializeBirthdays():
+def initializeBirthdays(birthdays):
+    birthdayFile = open("d:\Python Projects\BirthdayBot\BirthdayBot\\birthdays.txt")
     for line in birthdayFile:
         key, value = line.strip('\n').split(" ",1)
         birthdays[key] = value
+    birthdayFile.close()
+    return birthdays
 def idToName(name,birthdate):
     idtoNamesFile = open("d:\Python Projects\BirthdayBot\BirthdayBot\idstonames.txt", "a")
     idtoNamesFile.write(name+' '+birthdate+'\n')
-birthdays = {}
-birthdayFile = open("d:\Python Projects\BirthdayBot\BirthdayBot\\birthdays.txt")
-initializeBirthdays()
+    idtoNamesFile.close()
 
+def checkBirthdays():
+    global birthdayUser
+    global date
+    birthdays = {}
+    birthdates = []
+    print(datetime.today())
+    today = str(datetime.today().strftime('%m %d')).split(' ')
+    print(today)
+    f = open("d:\Python Projects\BirthdayBot\BirthdayBot\\birthdays.txt",'r')
+    content = f.read().splitlines()
+    for date in range(0,len(content),1):
+        print('birthday loop')
+        birthdates.append(content[date].split(' '))
+    for line in range(0,len(birthdates),1):
+        print("looping check")
+        print(line)
+        print(today)
+        print(birthdates[line])
+        currentBirthday = birthdates[line]
+        birthdayUser = currentBirthday[0]
+        currentBirthday = [currentBirthday[1],currentBirthday[2]]
+        print(currentBirthday)
+        if(currentBirthday == today):
+            print("it's someone's birthday")
+            return True
+    f.close()
+birthdays = {}
+birthdayUser = ''
+client = discord.Client
 token = os.getenv('token')
 print(birthdays)
-
+date = []
 bot = commands.Bot(command_prefix = '.')
 
 @bot.event
 async def on_ready():
     print('Birthday bot is ready')
+    called_once_a_day.start()
 
-@tasks.loop(hours=24)
+@tasks.loop(hours=2)
 async def called_once_a_day():
-
+    global birthdayUser
+    global date
+    message_channel_id = 854769754714669137
+    print("day check")
+    checkBirthdays()
+    print(date)
+    print("loopy")
+    message_channel = bot.get_channel(message_channel_id)
+    print(message_channel)
+    if(checkBirthdays()):
+        print("Birthday time for real")
+        await message_channel.send("@\everyone wish a very happy birthday to "+birthdayUser+"! Have an absolutely fantastic day 🎂🥳")
 @bot.command()
 async def addbirthday(message,*,nameandbirth : str):
     print(nameandbirth)
     birthdayFile = open("d:\Python Projects\BirthdayBot\BirthdayBot\\birthdays.txt","a")
-    birthdayFile.write('\n'+nameandbirth)
-    initializeBirthdays()
+    birthdayFile.write(nameandbirth+'\n')
     birthdate = nameandbirth.split(" ",1)
     idToName(message.author.name,birthdate[1])
+    birthdayFile.close()
     await message.channel.send("Birthday added!")
     print(birthdays)
 
@@ -44,8 +86,14 @@ async def addbirthday(message,*,nameandbirth : str):
 async def birthdays(ctx):
     birthdayFile = open("d:\Python Projects\BirthdayBot\BirthdayBot\idstonames.txt", "r")
     birthdaysList = birthdayFile.read()
+    birthdayFile.close()
     print(birthdaysList)
 
     await ctx.send('```'+birthdaysList+'```')
+@bot.command()
+async def initializeBot(ctx):
+
+    await ctx.send('Birthday loop started!')
 bot.run(token)
+
     
